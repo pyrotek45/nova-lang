448417efa208a86fd40ea71859ce1392960b4f04
pyrotek45 <pyrotek45_gaming@yahoo.com>
2023-11-01 16:46:27 +0000

    demug mode to step through asm

--- nova-lang/src/main.rs
+++ nova-lang/src/main.rs
@@ -149,6 +149,76 @@ fn main() {
                                 println!("Error: No file path specified");
                             }
                         }
+                        "dbg" => {
+                            if let Some(filepath) = std::env::args().nth(3) {
+                                let mut lexer = match lexer::new(&filepath) {
+                                    Ok(lexer) => lexer,
+                                    Err(error) => {
+                                        println!("{}", error);
+                                        exit(1)
+                                    }
+                                };
+                                let lexer_output = match lexer.tokenize() {
+                                    Ok(output) => {
+                                        // for t in output.clone().iter() {
+                                        //     println!("{:?}", t)
+                                        // }
+                                        output
+                                    }
+                                    Err(error) => {
+                                        error.show();
+                                        exit(1)
+                                    }
+                                };
+                                let mut parser = parser::new(&filepath);
+                                parser.input = lexer_output.clone();
+                                match parser.parse() {
+                                    Ok(()) => {
+                                        //dbg!(parser.ast.clone());
+                                    }
+                                    Err(error) => {
+                                        error.show();
+                                        exit(1)
+                                    }
+                                }
+                                let mut compiler = compiler::new();
+                                let mut vm = vm::new();
+                                let program = compiler
+                                    .compile_program(parser.ast, filepath, true, true, false);
+                                let asm = compiler.asm.clone();
+                                match program {
+                                    Ok(_) => {
+                                        // println!("Before optimization");
+                                        // dis.dis_asm(asm.clone());
+                                        // println!();
+                                        // let mut optimizer = optimizer::new();
+                                        // let optimized = optimizer.Optimize(asm.clone());
+                                        //println!("After optimization: {}", optimizer.optimizations);
+                                        // dis.dis_asm(asm.clone());
+                                        //println!("{}", rhexdump::hexdump(&assembler.output));
+
+                                        let mut assembler = assembler::new(asm);
+                                        assembler.assemble();
+                                        vm.state.program(assembler.output)
+                                    }
+                                    Err(error) => {
+                                        error.show();
+                                        exit(1)
+                                    }
+                                }
+                                match vm.run_debug() {
+                                    Ok(()) => {
+                                        //dbg!(vm.state.stack);
+                                    }
+                                    Err(error) => {
+                                        error.show();
+                                        exit(1)
+                                    }
+                                }
+                            } else {
+                                println!("Error: No file path specified");
+                            }
+                        }
                         "dis" => {
                             if let Some(filepath) = std::env::args().nth(3) {
                                 let mut lexer = match lexer::new(&filepath) {
@@ -263,6 +333,27 @@ fn main() {
                             }
                         }
 
+                        "runbin" => {
+                            if let Some(filepath) = std::env::args().nth(2) {
+                                let encoded = std::fs::read(filepath).unwrap();
+                                let program = bincode::deserialize(&encoded).unwrap();
+
+                                let mut vm = vm::new();
+                                vm.state.program(program);
+
+                                match vm.run() {
+                                    Ok(()) => {
+                                        //dbg!(vm.state.stack);
+                                    }
+                                    Err(error) => {
+                                        error.show();
+                                        exit(1)
+                                    }
+                                }
+                            } else {
+                                println!("Error: No file path specified");
+                            }
+                        }
                         _ => {
                             println!("Error: Unrecognized option {}", option);
                         }
@@ -270,6 +361,7 @@ fn main() {
                     None => todo!(),
                 }
             }
+
             "asm" => {
                 match std::env::args().nth(2) {
                     Some(option) => match option.as_str() {
@@ -353,35 +445,6 @@ fn main() {
                                     std::fs::write(format!("{}.nvb", outputname), encoded).unwrap();
                                 } else {
                                     println!("Error: No output name specified");
-                                }
-                            } else {
-                                println!("Error: No file path specified");
-                            }
-                        }
-                        _ => {}
-                    },
-                    None => todo!(),
-                }
-            }
-            "bin" => {
-                match std::env::args().nth(2) {
-                    Some(option) => match option.as_str() {
-                        "run" => {
-                            if let Some(filepath) = std::env::args().nth(3) {
-                                let encoded = std::fs::read(filepath).unwrap();
-                                let program = bincode::deserialize(&encoded).unwrap();
-
-                                let mut vm = vm::new();
-                                vm.state.program(program);
-
-                                match vm.run() {
-                                    Ok(()) => {
-                                        //dbg!(vm.state.stack);
-                                    }
-                                    Err(error) => {
-                                        error.show();
-                                        exit(1)
-                                    }
                                 }
                             } else {
                                 println!("Error: No file path specified");
