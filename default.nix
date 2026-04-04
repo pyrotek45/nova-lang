cac8e0af8b76ecabce74bc24f09ab450da6c4be5
pyrotek45 <pyrotek45_gaming@yahoo.com>
2026-04-04 18:14:12 -0400

    fix default.nix: wrap nova binary with LD_LIBRARY_PATH for X11/GL
    Without the nix-shell, the nova binary couldn't find libX11, libGL,
    etc. at runtime, causing GLFW init failure and segfault. Now uses
    makeWrapper + postFixup to embed the library path into the installed
    binary so raylib works outside the nix-shell.

--- default.nix
+++ default.nix
@@ -4,6 +4,17 @@ let
   nova-src = builtins.fetchTarball {
     url = "https://github.com/pyrotek45/nova-lang/archive/main.tar.gz";
   };
+
+  runtimeLibs = [
+    pkgs.libGL
+    pkgs.xorg.libX11
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXinerama
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXi
+    pkgs.wayland
+    pkgs.glfw
+  ];
 in
 pkgs.rustPlatform.buildRustPackage {
   pname = "nova";
@@ -18,20 +29,17 @@ pkgs.rustPlatform.buildRustPackage {
     pkgs.clang
     pkgs.pkg-config
     pkgs.llvmPackages_18.bintools
+    pkgs.makeWrapper
   ];
 
-  buildInputs = [
-    pkgs.libGL
-    pkgs.xorg.libX11
-    pkgs.xorg.libXrandr
-    pkgs.xorg.libXinerama
-    pkgs.xorg.libXcursor
-    pkgs.xorg.libXi
-    pkgs.wayland
-    pkgs.glfw
-  ];
+  buildInputs = runtimeLibs;
 
   LIBCLANG_PATH = "${pkgs.llvmPackages_18.libclang.lib}/lib";
+
+  postFixup = ''
+    wrapProgram $out/bin/nova \
+      --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath runtimeLibs}"
+  '';
 
   meta = with pkgs.lib; {
     description = "The Nova programming language";
